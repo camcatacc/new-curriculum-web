d21ff313b18dbbe1ad3744d5f2f661fcd59af78b
Camacho Cateura <david.camacho@accenture.com>
2021-11-12 13:34:01 +0100

    More optimizations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: ["pages/**/*.tsx", "components/**/*.tsx", "utils/**/*.tsx"],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {}
+	},
+	variants: {
+		extend: {}
+	},
+	plugins: []
+};
